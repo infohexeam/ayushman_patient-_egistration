f9d97369e33be715971552873798e8791983ea9e
Dhanyesh <dhanyesh98@gmail.com>
2024-05-31 15:22:31 +0530

    init

--- lib/api_services/urls.dart
+++ lib/api_services/urls.dart
@@ -1,7 +1,8 @@
 class AppUrls {
   static const String base = "patientapi.app.ayushmanayurvedic.in";
+  static const String maritalStatus = "api/marital_status";
   static const String register = "api/patient/tab_register";
   static const String gender = "api/gender";
   static const String bloodGroup = "api/blood-group";
-  static const String maritalStatus = "api/marital_status";
+ 
 }
